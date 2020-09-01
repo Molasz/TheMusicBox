9f10b638d350c13bc92f659a30b9237522dc224d
Marti Olasz <martiolasz@gmail.com>
2020-09-01 16:54:17 +0200

    Configure /band/:id route

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,12 @@ const debug = require('debug')('app');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const Band = require('./src/models/bandModel');
+
 const app = express();
 const { PORT: port } = process.env;
 
-mongoose.connect('mongodb://localhost/'); // collection name
+mongoose.connect('mongodb://localhost/TheMusicBox');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,6 +16,10 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('My server works!');
 });
+
+const bandRouter = require('./src/routes/bandRoutes')(Band);
+
+app.use('/band', bandRouter);
 
 /*
 const authRouter = require('./src/routes/heroRoutes')(User);
---
+++ backend/src/models/bandModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const bandModel = new Schema({
+  user: { type: String },
+  password: { type: String },
+  name: { type: String },
+  bio: { type: String },
+  logo: { type: String },
+  banner: { type: String },
+  discography: [
+    {
+      title: { type: String },
+      img: { type: String }
+    }
+  ],
+  concerts: [
+    {
+      date: { type: String },
+      city: { type: String }
+    }
+  ],
+  photos: [String]
+});
+
+module.exports = mongoose.model('bands', bandModel);
