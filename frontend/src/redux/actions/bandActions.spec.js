c0244b5b3f5fa2bf40482dcb59a38b147a112a21
Marti Olasz <martiolasz@gmail.com>
2020-09-02 15:58:44 +0200

    Create __mocks__ folder

--- frontend/src/redux/actions/bandActions.spec.js
+++ frontend/src/redux/actions/bandActions.spec.js
@@ -1,9 +1,22 @@
 import { getBand } from './bandActions';
 
+import { getBandSuccess } from '../actions/bandActions';
+import { error } from '../actions/errorAction';
+
+jest.mock('bandActions');
+
 describe('Band actions', () => {
-  it('should defined', () => {
-    expect(getBand).toBeDefined();
+  it('should call get band success', () => {
+    const mockDispatch = jest.fn;
+
+    getBand(0)(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(getBandSuccess);
   });
 
-  it('should throw error if fetch resolve with error', () => {});
+  it('should call error action if fetch resolve with error', () => {
+    const mockDispatch = jest.fn;
+
+    getBand(0)(mockDispatch);
+    expect(mockDispatch).toHaveBeenCalledWith(error);
+  });
 });
