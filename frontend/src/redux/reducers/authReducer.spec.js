f926233e184b41c8250566462525852389fe179e
Marti Olasz <martiolasz@gmail.com>
2020-09-06 13:11:14 +0200

    Auth reducer test 100%

---
+++ frontend/src/redux/reducers/authReducer.spec.js
@@ -0,0 +1,33 @@
+import types from '../actionTypes';
+import reducer from './authReducer';
+
+describe('Auth reducer', () => {
+  it('should return initial state', () => {
+    expect(reducer(undefined, {})).toEqual({});
+  });
+
+  it('should handle saveUser', () => {
+    const text = 'we are here';
+    const action = {
+      type: types.SAVE_USER,
+      payload: text
+    };
+    expect(reducer({}, action)).toEqual({ userIdentifier: text });
+  });
+
+  it('should handle removeUser', () => {
+    const action = {
+      type: types.REMOVE_USER
+    };
+    expect(reducer({}, action)).toEqual({ userIdentifier: null });
+  });
+
+  it('should handle getUser', () => {
+    const text = 'we are here';
+    const action = {
+      type: types.GET_USER,
+      payload: text
+    };
+    expect(reducer({}, action)).toEqual({ user: text });
+  });
+});
