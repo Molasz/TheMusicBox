2cd806eae84bb8b330c7fbe6a96f9b2b144331c2
Marti Olasz <martiolasz@gmail.com>
2020-09-08 20:58:44 +0200

    authAction test 100%

--- frontend/src/redux/actions/authActions.spec.js
+++ frontend/src/redux/actions/authActions.spec.js
@@ -1,5 +1,5 @@
 // Function to test
-import { getUser } from './authActions';
+import { getUser, addFollow, removeFollow } from './authActions';
 
 //Mock dependecies
 import axios from 'axios';
@@ -7,29 +7,82 @@ import axios from 'axios';
 jest.dontMock('./authActions');
 jest.mock('axios');
 
-describe('Auth actions', () => {
-  it('should call axios with /auth/:id', async () => {
-    axios.post.mockReturnValue(
-      new Promise((resolve) =>
-        resolve({
-          json: () => {}
-        })
-      )
-    );
-    const user = {
-      sub: 13,
-      nickname: 'bombasto'
-    };
-    await getUser(user)(jest.fn());
-    expect(axios.post.mock.calls[0][0]).toEqual('/auth/13');
+describe('Auth action', () => {
+  describe('getUser action', () => {
+    afterEach(() => jest.clearAllMocks());
+    it('should call axios with /auth/:id', async () => {
+      axios.post.mockReturnValue(
+        new Promise((resolve) =>
+          resolve({
+            json: () => {}
+          })
+        )
+      );
+      const user = {
+        sub: 13,
+        nickname: 'bombasto'
+      };
+      await getUser(user)(jest.fn());
+      expect(axios.post.mock.calls[0][0]).toEqual('/auth/13');
+    });
+
+    it('should call error action if axios throw error', async () => {
+      const dispatch = jest.fn();
+      axios.post.mockReturnValue(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+      await getUser(13)(dispatch);
+      expect(dispatch).toHaveBeenCalled();
+    });
   });
 
-  it('should call error action if axios throw error', async () => {
-    const dispatch = jest.fn();
-    axios.post.mockReturnValue(
-      new Promise((resolve, reject) => reject(new Error()))
-    );
-    await getUser(13)(dispatch);
-    expect(dispatch).toHaveBeenCalled();
+  describe('addFollow action', () => {
+    afterEach(() => jest.clearAllMocks());
+    it('should call axios with /auth/follow/:id', async () => {
+      axios.post.mockReturnValue(
+        new Promise((resolve) =>
+          resolve({
+            data: { following: { push: () => {} } }
+          })
+        )
+      );
+      const userId = 13;
+      await addFollow(userId)(jest.fn());
+      expect(axios.post.mock.calls[0][0]).toEqual('/auth/follow/13');
+    });
+
+    it('should call error action if axios throw error', async () => {
+      const dispatch = jest.fn();
+      axios.post.mockReturnValue(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+      await addFollow(13)(dispatch);
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+
+  describe('removeFollow action', () => {
+    afterEach(() => jest.clearAllMocks());
+    it('should call axios with /auth/followDelete/:id', async () => {
+      axios.post.mockReturnValue(
+        new Promise((resolve) =>
+          resolve({
+            data: { following: { filter: (callback) => callback() } }
+          })
+        )
+      );
+      const userId = 13;
+      await removeFollow(userId)(jest.fn());
+      expect(axios.post.mock.calls[0][0]).toEqual('/auth/followDelete/13');
+    });
+
+    it('should call error action if axios throw error', async () => {
+      const dispatch = jest.fn();
+      axios.post.mockReturnValue(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+      await removeFollow(13)(dispatch);
+      expect(dispatch).toHaveBeenCalled();
+    });
   });
 });
