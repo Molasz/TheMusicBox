9c9ce55a5187be3d6ac26b2c7f3507bc8150ff53
Marti Olasz <martiolasz@gmail.com>
2020-09-13 13:31:22 +0200

    Bandprofile onedit styles done

--- frontend/src/components/profile/bandProfile/bandBio/bandBio.jsx
+++ frontend/src/components/profile/bandProfile/bandBio/bandBio.jsx
@@ -29,112 +29,117 @@ function BandBio({
   return (
     <section className='bio'>
       <div className='bio__top'>
-        {Object.keys(editInfo).length === 0 &&
-        editInfo.constructor === Object ? (
-          <strong className='top__name'>{name}</strong>
-        ) : (
+        {editInfo.name !== undefined ? (
           <input
             type='text'
+            maxlength='50'
             value={editInfo.name}
             onChange={(event) => dispatch(bandEditName(event.target.value))}
-            className='top__input'
+            className='top__name input'
           />
+        ) : (
+          <strong className='top__name'>{name}</strong>
         )}
         <div className='top__region'>
-          {Object.keys(editInfo).length === 0 &&
-          editInfo.constructor === Object ? (
-            <>
-              <p className='region__city'>{city}</p> |
-              <p className='region__country'>{country}</p>
-            </>
-          ) : (
+          {editInfo.city !== undefined ? (
             <>
               <input
                 type='text'
+                maxlength='20'
                 value={editInfo.city}
                 onChange={(event) => dispatch(bandEditCity(event.target.value))}
-                className='region__city-input'
+                className='region__city input'
               />
 
               <input
                 type='text'
+                maxlength='20'
                 value={editInfo.country}
                 onChange={(event) =>
                   dispatch(bandEditCountry(event.target.value))
                 }
-                className='region__country-input'
+                className='region__country input'
               />
+            </>
+          ) : (
+            <>
+              <p className='region__city'>{city}</p> |
+              <p className='region__country'>{country}</p>
             </>
           )}
         </div>
       </div>
       <div className='bio__middle'>
-        {Object.keys(editInfo).length === 0 &&
-        editInfo.constructor === Object ? (
-          <p className='middle__bio'>{bio}</p>
-        ) : (
+        {editInfo.bio !== undefined ? (
           <textarea
             type='text'
             value={editInfo.bio}
             onChange={(event) => dispatch(bandEditBio(event.target.value))}
-            className='middle__text-area'
+            className='middle__bio text-area'
           />
+        ) : (
+          <p className='middle__bio'>{bio}</p>
         )}
       </div>
-      <div className='bio__bottom'>
-        {Object.keys(editInfo).length === 0 &&
-        editInfo.constructor === Object ? (
-          <>
-            <a
-              href={socialNetwork.twitter}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <TwitterIcon className='bottom-icon' />
-            </a>
-            <a
-              href={socialNetwork.facebook}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <FacebookIcon className='bottom-icon' />
-            </a>
-            <a
-              href={socialNetwork.instagram}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <InstagramIcon className='bottom-icon' />
-            </a>
-          </>
-        ) : (
-          <>
+      <div className={`bio__bottom ${editInfo.socialNetwork && 'edit'}`}>
+        <div className='bottom__container'>
+          <a
+            href={`http://${socialNetwork.twitter}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <TwitterIcon className='container__icon' />
+          </a>
+          {editInfo.socialNetwork?.twitter !== undefined && (
             <input
               type='text'
               value={editInfo.socialNetwork.twitter}
               onChange={(event) =>
                 dispatch(bandEditTwitter(event.target.value))
               }
-              className='bottom__input'
+              className='container__input input'
             />
+          )}
+        </div>
+        <div className='bottom__container'>
+          <a
+            href={`http://${socialNetwork.facebook}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FacebookIcon className='container__icon' />
+          </a>
+          {editInfo.socialNetwork?.facebook !== undefined && (
             <input
               type='text'
               value={editInfo.socialNetwork.facebook}
               onChange={(event) =>
                 dispatch(bandEditFacebook(event.target.value))
               }
-              className='bottom__input'
+              className='container__input input'
             />
+          )}
+        </div>
+        <div className='bottom__container'>
+          <a
+            href={`http://${socialNetwork.instagram}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <InstagramIcon className='container__icon' />
+          </a>
+
+          {editInfo.socialNetwork?.instagram !== undefined && (
             <input
               type='text'
               value={editInfo.socialNetwork.instagram}
               onChange={(event) =>
                 dispatch(bandEditInstagram(event.target.value))
               }
-              className='bottom__input'
+              className='container__input input'
             />
-          </>
-        )}
+          )}
+        </div>
       </div>
     </section>
   );
