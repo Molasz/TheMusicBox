cf598c9d2bad9d61d5e742dc4cb6ce3a98f5d465
Marti Olasz <martiolasz@gmail.com>
2020-09-15 20:20:11 +0200

    Band profile update logo and banner

--- backend/src/controllers/updateBand.js
+++ backend/src/controllers/updateBand.js
@@ -3,7 +3,12 @@ const callback = require('./callback');
 function updateBand(Band) {
   return (req, res) => {
     const { id } = req.params;
-    Band.findByIdAndUpdate(id, req.body, callback(res));
+    Band.findByIdAndUpdate(id, req.body)
+      .populate({
+        path: 'discography',
+        populate: { path: 'img', model: 'image' }
+      })
+      .exec(callback(res));
   };
 }
 
--- frontend/src/components/profile/bandProfile/bandHeader/bandHeader.jsx
+++ frontend/src/components/profile/bandProfile/bandHeader/bandHeader.jsx
@@ -4,18 +4,28 @@ import { toast } from 'react-toastify';
 
 import './bandHeader.scss';
 
-import { bandEdit } from '../../../../redux/actions/bandActions';
+import {
+  bandEdit,
+  sendBandEditInfo
+} from '../../../../redux/actions/bandActions';
+import { clearImage, uploadImage } from '../../../../redux/actions/infoActions';
 
 import onFollow from './onFollow';
-import onSave from './onSave';
 
 import Star from '@material-ui/icons/Grade';
 import Gear from '@material-ui/icons/Settings';
 import Save from '@material-ui/icons/Save';
 import PublicIcon from '@material-ui/icons/Public';
 
-function BandHeader({ band, editInfo, user, followers, dispatch }) {
+function BandHeader({ band, editInfo, user, followers, image, dispatch }) {
   const [isFollowing, setIsFollowing] = useState(null);
+
+  const [logo, setLogo] = useState();
+  const [banner, setBanner] = useState();
+
+  const [logoInput, setLogoInput] = useState(null);
+  const [bannerInput, setBannerInput] = useState(null);
+
   const publicAlert = () =>
     toast.error(
       'You need to fill name, biography, city and country before public the band',
@@ -48,10 +58,61 @@ function BandHeader({ band, editInfo, user, followers, dispatch }) {
     }
   }, [band._id, isFollowing, user, dispatch, editInfo]);
 
+  useEffect(() => {
+    if (image.identifier === 'logo') {
+      setLogo(image.path);
+    } else if (image.identifier === 'banner') {
+      setBanner(image.path);
+    }
+  }, [image]);
+
+  useEffect(() => {
+    debugger;
+    if (
+      (logo === null || typeof logo === 'string') &&
+      (banner === null || typeof banner === 'string')
+    ) {
+      const data = { ...editInfo };
+      if (logo) data.logo = logo;
+      if (banner) data.banner = banner;
+      dispatch(sendBandEditInfo(band._id, data));
+      dispatch(bandEdit({}));
+      dispatch(clearImage());
+    }
+  }, [logo, banner]);
+
   return (
     <section className='band-header'>
-      <img src={band.logo} alt='Logo' className='band-header__logo' />
-      <img src={band.banner} alt='Banner' className='band-header__banner' />
+      <img
+        src={band.logo}
+        alt='Logo'
+        className='band-header__logo'
+        onClick={() => {
+          if (editInfo.name) logoInput.click();
+        }}
+      />
+      <input
+        type='file'
+        name='file'
+        style={{ display: 'none' }}
+        ref={(fileInput) => setLogoInput(fileInput)}
+        onChange={(event) => setLogo(event.target.files[0])}
+      />
+      <img
+        src={band.banner}
+        alt='Banner'
+        className='band-header__banner'
+        onClick={() => {
+          if (editInfo.name) bannerInput.click();
+        }}
+      />
+      <input
+        type='file'
+        name='file'
+        style={{ display: 'none' }}
+        ref={(fileInput) => setBannerInput(fileInput)}
+        onChange={(event) => setBanner(event.target.files[0])}
+      />
       <div className='band-header__info'>
         <strong className='info__name'>
           {editInfo.name === undefined ? band.name : editInfo.name}
@@ -105,7 +166,19 @@ function BandHeader({ band, editInfo, user, followers, dispatch }) {
               <>
                 <Save
                   className='edit__save'
-                  onClick={(event) => onSave(event, band._id, editInfo)}
+                  onClick={(event) => {
+                    if (logo) {
+                      const img = new FormData();
+                      img.append('file', logo);
+                      dispatch(uploadImage(band._id, img, 'logo'));
+                    } else setLogo(null);
+
+                    if (banner) {
+                      const img = new FormData();
+                      img.append('file', banner);
+                      dispatch(uploadImage(band._id, img, 'banner'));
+                    } else setBanner(null);
+                  }}
                 />
                 <PublicIcon
                   className={`edit__public ${
@@ -136,7 +209,8 @@ function mapStateToProps(state) {
     followers: state.bandReducer.bandFollowers,
     user: state.authReducer.user,
     editInfo: state.bandReducer.editInfo,
-    band: state.bandReducer.band
+    band: state.bandReducer.band,
+    image: state.infoReducer.image
   };
 }
 
